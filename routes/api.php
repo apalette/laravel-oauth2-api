204dfd5f6478da1e724215e15d7dcc9dc68bab13
apalette <aurelie@codeuz.com>
2017-11-16 16:01:01 +0100

    User Login

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::group(['namespace' => 'api'], function () {
+    Route::post('login', '\App\Http\Controllers\Api\UserController@login');
 });
